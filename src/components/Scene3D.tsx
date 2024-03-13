51f7ca2545b54b5a83f4279fad8a858fa1769a8b
Adnan <you@example.com>
2024-03-13 02:14:41 +0100

    adding front page

--- src/components/Scene3D.tsx
+++ src/components/Scene3D.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import Experience from './3D/Experience';
-import { Environment } from '@react-three/drei';
+// import { Environment } from '@react-three/drei';
 import PostProcessing from './3D/PostProcessing';
 
 function Scene3D() {
@@ -11,9 +11,9 @@ function Scene3D() {
 			gl={{ antialias: false }} flat
 			shadows
 			style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1, backgroundColor: '#282828' }}>
-			<Environment files="/public/hdri/gradient_min_19.hdr" />
+			{/* <Environment files="/public/hdri/gradient_min_19.hdr" /> */}
 			<Experience />
-			<ambientLight intensity={2} />
+			<ambientLight intensity={5} />
 			<PostProcessing />
 			{/* <OrbitControls /> */}
 		</Canvas >
