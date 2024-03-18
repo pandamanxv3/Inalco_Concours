2e6c174c9726f9d1110629bfb0dab7b19fb02cae
Adnan <you@example.com>
2024-03-18 02:02:23 +0100

    debugging

--- src/components/Scene3D.tsx
+++ src/components/Scene3D.tsx
@@ -2,7 +2,7 @@ import { Canvas } from '@react-three/fiber';
 import Experience from './3D/Experience';
 // import { Environment } from '@react-three/drei';
 import PostProcessing from './3D/PostProcessing';
-import { Environment } from '@react-three/drei';
+// import { Environment } from '@react-three/drei';
 
 function Scene3D() {
 
@@ -12,7 +12,7 @@ function Scene3D() {
 			gl={{ antialias: false }} flat
 			shadows
 			style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1, backgroundColor: '#282828' }}>
-			<Environment files="/public/hdri/gradient_min_19.hdr" />
+			{/* <Environment files="/public/hdri/gradient_min_19.hdr" /> */}
 			<Experience />
 			<ambientLight intensity={3} />
 			<PostProcessing />
